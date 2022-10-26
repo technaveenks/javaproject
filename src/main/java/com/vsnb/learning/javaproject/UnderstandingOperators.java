26dc35a20b6d8b64f8f76941d787608f47a9a7be
technaveenks <tech.naveenks@gmail.com>
2022-10-26 07:00:26 +0530

    DataTYpes

---
+++ src/main/java/com/vsnb/learning/javaproject/UnderstandingOperators.java
@@ -0,0 +1,18 @@
+package com.vsnb.learning.javaproject;
+
+public class UnderstandingOperators {
+
+	public static void main(String[] args) {
+		int firstValue = 20;
+		int secondValue = 15;
+		
+		int sum = firstValue + secondValue;
+		System.out.println(sum);
+		
+		int substraction = firstValue - secondValue;
+		System.out.println(substraction);
+		
+		//  /, % , *
+		
+	}
+}
---
+++ src/main/java/com/vsnb/learning/javaproject/UnderstandingVariables.java
@@ -0,0 +1,17 @@
+package com.vsnb.learning.javaproject;
+
+public class UnderstandingVariables {
+
+	// primitive & non-primitive
+	// byte, short, int, long, float, double, boolean, char,
+	public static void main(String[] args) {
+		byte z = 20;
+		System.out.println(z);
+		boolean isTrue = true;
+		System.out.println(isTrue);
+		char value = 't';
+		System.out.println(value);
+		float values = 2.4f;
+		System.out.println(values);
+	}
+}
